4ee9ede7f0668aeb5f281705db75251edf0e625d
Seliverstov Vitaly <seliverstov_vitaly@mail.ru>
2019-11-15 21:01:32 +0400

    Added BigDecimal numbers type and processing of incorrest expression

--- src/com/seliverstov/calculator/ReverseExpression.java
+++ src/com/seliverstov/calculator/ReverseExpression.java
@@ -4,7 +4,7 @@ import java.util.Stack;
 
 public class ReverseExpression {
 
-    public static  String transform(String exp) {
+    public static  String transform(String exp) throws Exception {
         String calculation = "";
         Stack<Character> stack = new Stack<>();
         int priority;
@@ -13,39 +13,40 @@ public class ReverseExpression {
         String correctedExpression = MathOperation.correct(exp);
 
         for (char ch: correctedExpression.toCharArray()) {
-            if (Character.isDigit(ch) || ch == '.') {
-                calculation += ch;
-            }
-            if (MathOperation.OPERATION_MAP.containsKey(ch)) {
-                priority = MathOperation.OPERATION_MAP.get(ch).getPriority();
 
-                if (priority == 0) {
-                    while(MathOperation.OPERATION_MAP.get(stack.peek()).getPriority() != 1) {
-                        calculation += stack.pop();
-                    }
-                    stack.pop();
-                }
+            if (MathOperation.OPERATION_MAP.containsKey(ch) || Character.isDigit(ch) || ch == '.') {
 
-                if (priority == 1) {
-                    stack.push(ch);
-                }
+                if (Character.isDigit(ch) || ch == '.') calculation += ch;
+                if (MathOperation.OPERATION_MAP.containsKey(ch)) {
+                    priority = MathOperation.OPERATION_MAP.get(ch).getPriority();
 
-                if (priority >= 2) {
-                    calculation += ' ';
-                    while (!stack.empty()) {
-                        if (MathOperation.OPERATION_MAP.get(stack.peek()).getPriority() >= priority)
-                        {
+                    if (priority == 0) {
+                        while (MathOperation.OPERATION_MAP.get(stack.peek()).getPriority() != 1) {
                             calculation += stack.pop();
-                        } else break;
+                        }
+                        stack.pop();
                     }
-                    stack.push(ch);
+
+                    if (priority == 1) {
+                        stack.push(ch);
+                    }
+
+                    if (priority >= 2) {
+                        calculation += ' ';
+                        while (!stack.empty()) {
+                            if (MathOperation.OPERATION_MAP.get(stack.peek()).getPriority() >= priority) {
+                                calculation += stack.pop();
+                            } else break;
+                        }
+                        stack.push(ch);
+                    }
                 }
+            } else {
+                throw new Exception("Incorrect expression");
             }
         }
-        while (!stack.empty()) {
-            calculation += stack.pop();
+        while (!stack.empty()) calculation += stack.pop();
 
-        }
         return calculation;
     }
 }
